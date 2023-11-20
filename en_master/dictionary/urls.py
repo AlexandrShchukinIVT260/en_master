7bfc25a4613d5daf4b6ea131bbaba8a690f79936
MontaGGGne <100992858+AlexandrShchukinIVT260@users.noreply.github.com>
2023-11-20 19:27:44 +0300

    shchukin: en_master with git and docker

--- en_master/dictionary/urls.py
+++ en_master/dictionary/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.DictHome.as_view(), name='dict_home'),
     path('add-word', views.WordCreateView.as_view(), name='word_create'),
-    # path('<int:pk>', views.WordDetailView.as_view(), name='word_detail'),
+    path('<int:pk>', views.WordDetailView.as_view(), name='word_detail'),
     path('<int:pk>/update-word', views.WordUpdateView.as_view(), name='word_update'),
     path('<int:pk>/delete-word', views.WordDeleteView.as_view(), name='word_delete')
 ]
--- en_master/dictionary/views.py
+++ en_master/dictionary/views.py
@@ -13,12 +13,27 @@ from .forms import *
 class DictHome(LoginRequiredMixin, DataMixin, ListView):
     model = WordTranslate
     template_name = 'dictionary/dictionary.html'
-    context_object_name = 'dict'
+    context_object_name = 'words'
 
     def get_context_data(self, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         c_def = self.get_user_context(title="Словарь")
         return dict(list(context.items()) + list(c_def.items()))
+
+
+class WordDetailView(LoginRequiredMixin, DataMixin, DetailView):
+    model = WordTranslate
+    form_class = WordForm
+    template_name = 'dictionary/detail_word.html'
+    context_object_name = 'detail'
+    
+    def get_context_data(self, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Обзор слова")
+        return dict(list(context.items()) + list(c_def.items()))
+    
+    def get_success_url(self):
+        return reverse_lazy('dict_home')
 
 
 class WordCreateView(LoginRequiredMixin, DataMixin, CreateView):
